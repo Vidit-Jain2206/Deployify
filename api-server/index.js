75cf83f7911237bd5521749333882037250555f7
Vidit-Jain2206 <jainvidit203@gmail.com>
2025-01-09 18:28:06 +0530

    test

--- api-server/index.js
+++ api-server/index.js
@@ -96,7 +96,7 @@ app.post("/deploy", async (req, res) => {
     });
 });
 app.get("/", (req, res) => {
-  res.send("Deployify Server");
+  res.send("Deployify Api-Server");
 });
 app.listen(9000, () => {
   console.log("Server started on port 9000");
